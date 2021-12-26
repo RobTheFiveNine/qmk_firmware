ed5fba66842da1010f6e769a97241044529f4d37
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2021-12-26 21:37:43 +0000

    Fix config

--- keyboards/preonic/keymaps/RobTheFiveNine/config.h
+++ keyboards/preonic/keymaps/RobTheFiveNine/config.h
@@ -16,13 +16,9 @@
 
 #pragma once
 
-#include "color.h"
-
 #ifdef AUDIO_ENABLE
-#    define AUDIO_CLICKY
 #    define AUDIO_INIT_DELAY
 #    define STARTUP_SONG SONG(PREONIC_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
@@ -30,27 +26,7 @@
 
 #define MUSIC_MASK (keycode != KC_NO)
 
-/*
- * MIDI options
- */
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
 #define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_DEFAULT_HUE 191
-/* #define RGBLIGHT_DEFAULT_MODE RGB_MODE_PLAIN */
